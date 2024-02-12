edaafd8f88410642d220a044740e1617efbcfb5c
Abhishek-mehata <mehtaabishekkumar99@gmail.com>
2024-02-12 18:47:55 +0545

    For Fun

--- robotics_arm.ino
+++ robotics_arm.ino
@@ -19,10 +19,19 @@ int bot_m1=360;// In degree For Motor A
 int bot_m2=360;// In degree For Motor B
 int bot_m3=360;// In degree For Motor C
 
+//  Variables and data assigning  for LDR resistor
+const int ldr_pin=A0;
+const int ldr_midvalue=500;//ny analog reading from the LDR that is greater than 500 will be considered as indicating 
+// a light condition, and any reading less than or equal to 500 will be considered as indicating a dark condition.
+int ldr_sensorvalue;
+int ldr_binaryvalue;
+
+
 void setup() 
 {
   // put your setup code here, to run once:
   // Serial.begin();
+  Serial.begin(9600); // Initialize serial communication
   s1.attach(9);
   s2.attach(10);
   s3.attach(11);
@@ -62,6 +71,9 @@ void container_in_bot()
 
 void loop() {
   // put your main code here, to run repeatedly:
+  ldr_sensorvalue = analogRead(ldr_pin); // Read analog value from LDR
+  ldr_binaryvalue = ((ldr_binaryvalue >ldr_midvalue) ? 1 : 0); // Convert to digital
+  Serial.println(ldr_binaryvalue); // Send digital value over serial
   container_in_farm();
   container_in_air();
   container_in_bot();
